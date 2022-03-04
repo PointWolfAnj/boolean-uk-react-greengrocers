d8c932f48ef3b528723f42bf60bb1d4875201bf6
PointWolfAnj <anojan31@gmail.com>
2022-03-04 16:50:00 +0000

    C

--- src/App.js
+++ src/App.js
@@ -38,11 +38,65 @@ export default function App() {
     }
   }
   
-
-  // below is a combination about both of the above functions in one
   function addToCart(item) {
     cart.some(element => element.id === item.id)
       ? onIncreaseClick(item)
       : addNewItem(item)
   }
 
+  const total = cart.reduce(
+    (accumulator, current) => accumulator + current.price * current.quantity,
+    0
+  )
+  return (
+    <>
+      <header id="store">
+        <h1>Greengrocers</h1>
+        <ul className="item-list store--item-list">
+          {/* rendering the store */}
+          {store.map(items => {
+            return <Store addToCart={addToCart} items={items} />
+          })}
+        </ul>
+      </header>
+      <main id="cart">
+        <h2>Your Cart</h2>
+        <div className="cart--item-list-container">
+          <ul className="item-list cart--item-list">
+            {cart.map(addedItems => {
+              return (
+                <Cart
+                  onDecreaseClick={onDecreaseClick}
+                  onIncreaseClick={onIncreaseClick}
+                  addedItems={addedItems}
+                />
+              )
+            })}
+          </ul>
+        </div>
+        <div className="total-section">
+          <div>
+            <h3>Total</h3>
+          </div>
+          <div>
+            
+            <span className="total-number">£ {total.toFixed(2)}</span>
+          </div>
+        </div>
+      </main>
+      <div>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/icongeek26"
+          title="Icongeek26"
+        >
+          Icongeek26
+        </a>
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </>
+  )
+}
